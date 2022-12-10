ab11ab872a2299648dcdb7c996872a293f66133d
Archanapudayan <atchanaudayan64534@gmail.com>
2022-12-10 11:57:52 +0530

    viewEmployee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewEmployeeComponent } from './view-employee/view-employee.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewEmployeeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
